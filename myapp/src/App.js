ad52a20c2c10e3e07c1aaf3ff5f8b12678415ba9
alamin <alaminwww07@gmail.com>
2018-12-25 20:59:53 +0600

    Food Rating App

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import View from './View';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <View />
+      </div>
+    );
+  }
+}
+
+export default App;
